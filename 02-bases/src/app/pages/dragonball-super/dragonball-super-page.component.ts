2b2cccfbafb9fdc075aa1241d6178fee05968a31
Fabián Gómez Campo <fabian.campo@icloud.com>
2025-05-12 21:27:28 +0200

    feat: angular

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -6,14 +6,17 @@ interface Character {
     power: number
 }
 @Component({
-    selector: 'app-dragonball-page',
+    selector: 'dragonball-super',
     imports: [],
-    templateUrl: './dragonball-page.component.html',
+    templateUrl: './dragonball-super-page.component.html',
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
     name = signal('')
     power = signal(0)
-    characters = signal<Character[]>([{ id: 1, name: 'Goku', power: 9001 }])
+    characters = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 9001 },
+        { id: 2, name: 'Vegeta', power: 8001 },
+    ])
 
     addCharacter = () => {
         if (!this.name || !this.power() || this.power() <= 0) {
