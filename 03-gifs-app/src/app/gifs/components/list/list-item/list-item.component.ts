28d22d0026ce3468a4427850f6c937efe5fcf8a2
Fabián Gómez Campo <fabian.campo@icloud.com>
2025-05-14 12:06:11 +0200

    feat: sestion 6 finished

---
+++ 03-gifs-app/src/app/gifs/components/list/list-item/list-item.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+
+@Component({
+    selector: 'gifs-list-item',
+    imports: [],
+    templateUrl: './list-item.component.html',
+})
+export class ListItemComponent {
+    imageUrl = input.required<string>()
+}
---
+++ 03-gifs-app/src/app/gifs/components/list/list.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { ListItemComponent } from './list-item/list-item.component'
+
+@Component({
+    selector: 'gifs-list',
+    imports: [ListItemComponent],
+    templateUrl: './list.component.html',
+})
+export class ListComponent {
+    gifs = input.required<string[]>()
+}
