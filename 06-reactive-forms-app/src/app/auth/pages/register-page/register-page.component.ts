8548a6789bad90161299bfc080c51587b777507f
Fabián Gómez Campo <fgomezc@uxcale.com>
2025-06-25 17:03:13 +0200

    feat: changes

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,74 @@
 import { JsonPipe } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms'
+import { FormUtils } from '../../../utils/form-utils'
 
 @Component({
     selector: 'app-register-page',
-    imports: [JsonPipe],
+    imports: [JsonPipe, ReactiveFormsModule],
     templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+    private fb = inject(FormBuilder)
+
+    formUtils = FormUtils
+
+    form: FormGroup = this.fb.group(
+        {
+            name: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern(this.formUtils.namePattern),
+                ],
+            ],
+            email: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern(this.formUtils.emailPattern),
+                ],
+                [this.formUtils.checkingServerResponse],
+            ],
+            username: [
+                '',
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.pattern(this.formUtils.notOnlySpacesPattern),
+                    this.formUtils.notStrider,
+                ],
+            ],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            password2: ['', [Validators.required, Validators.minLength(6)]],
+        },
+        {
+            Validators: [
+                this.formUtils.isFieldOneEqualFieldTwo('password', 'password2'),
+            ],
+        }
+    )
+
+    onSubmit() {
+        this.form.markAllAsTouched()
+
+        console.log(this.form.value)
+    }
+
+    isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: AbstractControl) => {
+            const field1Value = formGroup.get(field1)?.value
+            const field2Value = formGroup.get(field2)?.value
+
+            return field1Value === field2Value
+                ? null
+                : { passwordsNotEqual: true }
+        }
+    }
+}
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,6 +1,19 @@
-import { Form, FormArray, FormGroup, ValidationErrors } from '@angular/forms'
+import {
+    AbstractControl,
+    Form,
+    FormArray,
+    FormGroup,
+    ValidationErrors,
+} from '@angular/forms'
 
+async function sleep() {
+    return new Promise((resolve) => setTimeout(resolve, 2000))
+}
 export class FormUtils {
+    static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)'
+    static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+    static notOnlySpacesPattern = '^[a-zA-Z0-9]+$'
+
     static isValidField(form: FormGroup, field: string): boolean {
         return !!form.controls[field].errors && form.controls[field].touched
     }
@@ -15,7 +28,7 @@ export class FormUtils {
                 case 'min':
                     return `El valor mínimo es ${errors['min'].min}`
                 default:
-                    return null
+                    return 'Error de validación desconocido'
             }
         }
         return null
@@ -26,6 +39,17 @@ export class FormUtils {
         const errors = form.controls[field].errors ?? {}
 
         return FormUtils.getTextError(errors)
+    }
+
+    static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: AbstractControl) => {
+            const field1Value = formGroup.get(field1)?.value
+            const field2Value = formGroup.get(field2)?.value
+
+            return field1Value === field2Value
+                ? null
+                : { passwordsNotEqual: true }
+        }
     }
 
     static onSave(form: FormGroup): void {
@@ -57,5 +81,29 @@ export class FormUtils {
         const errors = formArray.controls[index].errors ?? {}
 
         return FormUtils.getTextError(errors)
+    }
+
+    static async checkingServerResponse(
+        control: AbstractControl
+    ): Promise<ValidationErrors | null> {
+        await sleep()
+
+        const formValue = control.value
+
+        if (formValue === 'hola@mundo.com') {
+            return { emailTaken: true }
+        }
+
+        return null
+    }
+
+    static notStrider(control: AbstractControl): ValidationErrors | null {
+        const value = control.value
+
+        if (value === 'strider') {
+            return { notStrider: true }
+        }
+
+        return null
     }
 }
