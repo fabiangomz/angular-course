dd1af5fbe9160dadba3295bbf5fbf87041752f2a
Fabián Gómez Campo <fgomezc@uxcale.com>
2025-06-26 10:25:27 +0200

    feat: section 15 finished

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,7 +1,7 @@
 import { JsonPipe } from '@angular/common'
-import { Component, inject, signal } from '@angular/core'
+import { Component, effect, inject, signal } from '@angular/core'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
-import { count } from 'rxjs'
+import { count, filter, switchMap, tap } from 'rxjs'
 import { CountryService } from '../../services/country.service'
 import { Country } from '../../interfaces/country.interface'
 
@@ -24,4 +24,33 @@ export class CountryPageComponent {
         country: ['', Validators.required],
         border: ['', Validators.required],
     })
+
+    onFormChanged = effect((onCleanup) => {
+        const regionSubscription = this.onRegionChanged()
+        const countrySubscription = this.onCountryChanged()
+
+        onCleanup(() => {
+            regionSubscription?.unsubscribe()
+            countrySubscription?.unsubscribe()
+        })
+    })
+
+    onRegionChanged() {
+        return this.form
+            .get('region')!
+            .valueChanges.pipe(
+                tap(() => this.form.get('country')!.setValue('')),
+                tap(() => this.form.get('border')!.setValue('')),
+                tap(() => {
+                    this.borders.set([])
+                    this.countriesByRegion.set([])
+                }),
+                switchMap((region) =>
+                    this.countryService.getCountriesByRegion(region ?? '')
+                )
+            )
+            .subscribe((countries) => {
+                this.countriesByRegion.set(countries)
+            })
+    }
 }
