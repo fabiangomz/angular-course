4b4481766de65b9558d7f78fb591ea71efa3bd4f
Fabián Gómez Campo <fgomezc@uxcale.com>
2025-07-01 10:05:26 +0200

    feat: section 17 init

---
+++ 07-lifecycle-hooks/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core'
+import { RouterLink, RouterLinkActive } from '@angular/router'
+
+@Component({
+    selector: 'app-navbar',
+    imports: [RouterLink, RouterLinkActive],
+    templateUrl: './navbar.component.html',
+    styles: `
+nav {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+}
+
+.active {
+  color: red;
+  font-weight: bold
+}
+  `,
+})
+export class NavbarComponent {}
